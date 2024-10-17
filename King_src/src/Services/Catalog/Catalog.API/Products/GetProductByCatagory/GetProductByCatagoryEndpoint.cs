9820aa4344d1237fc8aebd84b070e23f78911d70
KingArceus <khangtpc@gmail.com>
2024-10-18 01:52:02 +0700

    Complete  Endpoint and Handler for Catalog API

---
+++ King_src/src/Services/Catalog/Catalog.API/Products/GetProductByCatagory/GetProductByCatagoryEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.GetProductByCatagory
+{
+    //public record GetProductByCatagoryRequest();
+    public record GetProductByCatagoryResponse(IEnumerable<Product> Products);
+    public class GetProductByCatagoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(category));
+                var response = result.Adapt<GetProductByCatagoryResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductByCategory")
+            .Produces<GetProductByCatagoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetProductByCategory")
+            .WithDescription("Get Product By Category");
+        }
+    }
+}
