7c4b09d11e3e63cbceb743f5a35c96cd0276cf7e
KingArceus <khangtpc@gmail.com>
2024-10-19 02:14:22 +0700

    Finish Catalog.API

--- King_src/src/Services/Catalog/Catalog.API/Products/GetProductByCatagory/GetProductByCatagoryHandler.cs
+++ King_src/src/Services/Catalog/Catalog.API/Products/GetProductByCatagory/GetProductByCatagoryHandler.cs
@@ -2,12 +2,11 @@
 {
     public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
     public record GetProductByCategoryResult(IEnumerable<Product> Products);
-    internal class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger)
+    internal class GetProductByCategoryHandler(IDocumentSession session)
         : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
     {
         public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation($"GetProductByCatagoryHandler. Handler request by {query}");
             var products = await session.Query<Product>()
                 .Where(p => p.Category.Contains(query.Category))
                 .ToListAsync();
--- King_src/src/Services/Catalog/Catalog.API/Program.cs
+++ King_src/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,14 +1,40 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
+var program = typeof(Program).Assembly;
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(program);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 builder.Services.AddMarten(opts => opts.Connection(builder.Configuration.GetConnectionString("Database")!))
     .UseLightweightSessions();
+builder.Services.AddValidatorsFromAssembly(program);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 var app = builder.Build();
 
 // Configure HTTP request pipline
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
