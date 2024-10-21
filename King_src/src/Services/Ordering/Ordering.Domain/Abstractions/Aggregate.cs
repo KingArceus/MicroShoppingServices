f0154fd243dd69090b8e6327bdd48b7e02b19ee1
KingArceus <khangtpc@gmail.com>
2024-10-22 00:17:09 +0700

    FInish build models for Ordering.Domain

---
+++ King_src/src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyList<IDomainEvent> Events => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent) 
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvent()
+        {
+            IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+
+            _domainEvents.Clear();
+
+            return dequeuedEvents;
+        }
+    }
+}
