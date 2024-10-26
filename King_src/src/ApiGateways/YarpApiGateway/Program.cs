820bf7392d1001333b3a28938cfa7bc27e6fc65c
KingArceus <khangtpc@gmail.com>
2024-10-27 00:19:32 +0700

    Finish YarpApi

---
+++ King_src/src/ApiGateways/YarpApiGateway/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adđ Services
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddRateLimiter(limiterOptions =>
+{
+    limiterOptions.AddFixedWindowLimiter("fixed", option =>
+    {
+        option.Window = TimeSpan.FromSeconds(10);
+        option.PermitLimit = 5;
+    });
+});
+
+var app = builder.Build();
+
+// Use Methods
+app.UseRateLimiter();
+
+app.MapReverseProxy();
+
+app.Run();
